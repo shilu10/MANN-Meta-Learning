d30fa1f8a60c4c335214281f34e3adb998ef293f
Shilu <shilu842003@gmail.com>
2023-11-10 18:52:24 +0530

    added experiement notebook

--- custom_trainer.py
+++ custom_trainer.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from tensorflow.keras import layers
 import datetime
 from tqdm import tqdm
-from data_loader import DataGenerator
+from dataloader import DataGenerator
 
 
 class Trainer:
